6d0af42e2cb51b1e53421c4d3861a5a6b9f74e6a
iiieezaakk <iiieezaakk@gmail.com>
2019-07-02 20:28:42 +0800

    Revert "removed the unity project"
    This reverts commit 8d94bd7dc8951bdb8d3bf0f0bc61aa0aef56217c.

---
+++ TwinStick Base Defence/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position;
+    }
+    
+    void Update()
+    {
+        transform.position = GameController.players[0].transform.position + offset;
+    }
+}
